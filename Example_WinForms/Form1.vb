188b97bf32009c939525bb8a8b195c10fe802a3c
Cory Smith <dualbrain@msn.com>
2024-04-22 12:51:59 -0500

    Upgraded to .NET 8.

--- Example_WinForms/Form1.vb
+++ Example_WinForms/Form1.vb
@@ -1,36 +1,33 @@
-﻿Imports System.Drawing
-
-Imports QB.Console
-Imports QB.ConsoleEx
-Imports QB.Core
-Imports QB.Development
-Imports QB.Devices
-Imports QB.File
-Imports QB.LineOption
-Imports QB.Multimedia
-Imports QB.PutOption
-Imports QB.Video
+﻿Imports QB.Video
 
 Public Class Form1
 
   Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    QB.Video.Init(Me, PictureBox1)
+    Init(Me, PictureBox1)
 
-    QB.Video.SCREEN(9)
+    SCREEN(9)
+
+    Dim px = Rnd() * 640
+    Dim py = Rnd() * 350
+
+    Dim c = 1
 
     Do
 
       Dim x = Rnd() * 640
       Dim y = Rnd() * 350
-      Dim r = Rnd() * 100
+      'Dim r = Rnd() * 100
 
-      CIRCLE(x, y, r)
+      'CIRCLE(x, y, r)
+      'LINE(px, py, x, y, c, BF)
+      LINE(px, py, x, y, c)
 
-      Await Task.Delay(1)
+      px = x : py = y : c += 1 : If c > 15 Then c = 1
+
+      Await Task.Delay(1) ' Let WinForms "refresh".
 
     Loop
-
 
   End Sub
 
